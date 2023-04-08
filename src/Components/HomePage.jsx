2ca554492cb4b9d8465d65ef1ff73485a3dc34e7
eieieieiedododo <tkdduq7071@gmail.com>
2023-04-08 16:08:51 +0900

    ver07

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import {Row, Col}from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 const HomePage = () => {
-  return (
-    <Row className='my-3'>
-        <Col>
-            <h1 className='text-center'>홈</h1>
-        </Col>
-    </Row>
-   
-  )
+    return (
+        <Row className='my-3'>
+            <Col>
+                <h1 className='text-center'>홈</h1>
+            </Col>
+        </Row>
+
+    )
 }
 
 export default HomePage
--- src/Components/RouterPage.jsx
+++ src/Components/RouterPage.jsx
@@ -11,26 +11,26 @@ import BookPage from './BookPage';
 
 const RouterPage = () => {
   return (
-    <>  
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Link to="/book">LOGO</Link>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll>
-                <Link to="/">홈</Link>
-                <Link to="/book">도서검색</Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <Switch>
-        <Route path="/" component={HomePage} exact={true}/>
-        <Route path="/" component={BookPage}/>
-    </Switch>
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container fluid>
+          <Link to="/book">LOGO</Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll>
+              <Link to="/">홈</Link>
+              <Link to="/book">도서검색</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Switch>
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/" component={BookPage} />
+      </Switch>
     </>
   )
 }
